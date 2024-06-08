1f54edff08a9f65309a585c2c9cb0a3bfe77409e
darkersoldierBsk <134071847+darkersoldierBsk@users.noreply.github.com>
2024-06-09 02:20:42 +0300

    solveThisFirst.cpp

---
+++ solveThisFirst.cpp
@@ -0,0 +1,31 @@
+/*
+Eren is interested in collecting controllers of old video game consoles and found a thrift shop that sells 2 boxes of controllers, for Atari and for PlayStation 1.
+It's written on each box how many controllers it contains. 
+To figure out what to pay, Eren needs to calculate how many controllers there are in the boxes in total.
+You are given two numbers, which are the numbers of controllers in boxes.
+
+INPUT FORMAT
+First line contains 2 integers - a and b
+
+OUTPUT FORMAT
+Print a + b in single line 
+
+Constraints
+0 ≤ a, b ≤ 10^5
+
+
+SAMPLE INPUT  |  SAMPLE OUTPUT
+98761 36837   |     135598
+
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+	  return 0;
+}
